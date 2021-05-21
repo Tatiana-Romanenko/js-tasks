9805960522cd97093523216bc499dba51682f3c0
Tatiana Romanenko <t.womanly@gmail.com>
2021-05-22 00:03:23 +0300

    Модуль 2

--- js/tasks.js
+++ js/tasks.js
@@ -309,7 +309,7 @@ switch (country) {
 
 //===========Task 30===========
 
-function getNameLength(name) {
+/*function getNameLength(name) {
   let length;
   const message = `Name ${name} is ${name.length} characters long`; // Change this line
 
@@ -366,5 +366,413 @@ message = message.toLowerCase();
   
   // Change code above this line
   return result;
+}*/
+
+
+//===========MODUL_2=============
+
+//===========Task 1===========
+
+/*function checkAge(age) {
+  if (age >= 18) {
+    return "You are an adult";
+  }
+
+  return "You are a minor";
+}*/
+
+//===========Task 2===========
+
+/*function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  
+
+
+  if (password === ADMIN_PASSWORD) {
+    return "Welcome!";
+  }
+
+  return "Access denied, wrong password!";
+ 
 }
 
+checkPassword("mangohackzor");
+checkPassword("polyhax");
+checkPassword("jqueryismyjam");
+
+
+//===========Task 3===========
+
+
+function checkStorage(available, ordered) {
+  
+  if (ordered <= available) {
+    return "The order is accepted, our manager will contact you";
+  }  
+  if (ordered === 0) {
+    return "Your order is empty!";
+  } 
+  
+  return "Your order is too large, not enough goods in stock!";
+}
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 250));
+console.log(checkStorage(150, 0));
+
+  
+ 
+//===========Task 4===========
+const fruits = ["apple", "plum", "pear", "orange"];
+console.log(fruits);
+
+
+//===========Task 5===========
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];*/
+
+//===========Task 6===========
+/*const fruits = ["apple", "plum", "pear", "orange"];
+
+// Write your code under this line
+fruits[1] = "peach";
+fruits[3] = "banana";
+
+console.log(fruits);*/
+
+//===========Task 7===========
+/*const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const fruitsArrayLength = fruits.length;
+
+console.log(fruitsArrayLength);
+
+//===========Task 8===========
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+
+//===========Task 9===========
+
+function getExtremeElements(array) {
+  // Change code below this line
+  
+ const firstElement = array[0];
+ const lastElementIndex = array.length - 1;
+ const lastElement = array[lastElementIndex];
+  
+  return array = [firstElement, lastElement];
+
+  // Change code above this line
+}*/
+
+//===========Task 10===========
+
+/*function splitMessage(message, delimeter) {
+  let words;
+  // Change code below this line
+words = message.split(delimeter);
+  // Change code above this line
+  return words;
+}
+console.log(splitMessage("Mango hurries to the train", " "));
+console.log(splitMessage("Mango", ""));
+console.log(splitMessage("best_for_week", "_"));
+
+//===========Task 11===========
+
+function calculateEngravingPrice(message, pricePerWord) {
+   // Change code below this line
+
+  
+message = message.split(" ").length;
+  
+const totalPrice = message * pricePerWord;
+  
+  return totalPrice;
+  
+   // Change code above this line
+}
+
+//===========Task 12===========
+function makeStringFromArray(array, delimeter) {
+  let string;
+  // Change code below this line
+
+string = array.join(delimeter);
+
+  // Change code above this line
+  return string;
+}
+
+//===========Task 13===========
+
+function slugify(title) {
+  // Change code below this line
+
+slug = title.toLowerCase().split(" ").join("-");
+  
+  return slug;
+
+  // Change code above this line
+}
+
+//===========Task 14===========
+
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// Change code below this line
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
+const lastThreeEls = fruits.slice(-3);
+
+//===========Task 15===========
+
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+
+const allClients = oldClients.concat(newClients); */
+
+
+//===========Task 16===========
+
+/*function makeArray(firstArray, secondArray, maxLength) {
+  // Change code below this line
+
+  const array = firstArray.concat(secondArray);
+  const arrayLength = array.length;
+  
+  if (arrayLength > maxLength) {
+    return array.slice(0, maxLength);
+  }
+  
+  return array;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+
+//===========Task 17===========
+const start = 3;
+const end = 7;
+
+for (let i = start; i <= end; i += 1) { // Change this line
+  console.log(i);
+}
+
+//===========Task 18===========
+function calculateTotal(number) {
+ // Change code below this line
+
+  let total = 0;
+  
+  for (let i = 1; i <= number; i += 1) { 
+  total += i; 
+}
+  
+return total;
+
+  
+}
+
+//===========Task 19===========
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length; i += 1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
+
+//===========Task 20===========
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+for (const item of order) {
+total += item;
+}
+  
+  // Change code above this line
+  return total;
+}
+
+//===========Task 21===========
+
+function findLongestWord(string) {
+  // Change code below this line
+  const arrayString = string.split(" ");
+ let longestWord = arrayString[0];
+  for (let i = 0; i < arrayString.length; i += 1) {
+  if (longestWord.length < arrayString[i].length) {
+  longestWord = arrayString[i];
+  }
+  }
+  return longestWord;
+  // Change code above this line
+}
+
+//===========Task 22===========
+
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+for (let i = min; i <= max; i += 1) { // Change this line
+  numbers.push(i);
+}
+  // Change code above this line
+  return numbers;
+}
+
+//===========Task 23===========
+
+function filterArray(numbers, value) {
+   // Change code below this line
+
+  const result = [];
+  
+  for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > value) {
+  result.push(numbers[i]);
+  }
+  }
+  
+  return result;
+  // Change code above this line
+}
+
+//===========Task 24===========
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit); // Change this line
+}
+
+//===========Task 25===========
+
+function getCommonElements(array1, array2) {
+  // Change code below this line
+const uniqieElements = [];
+  
+  for (const element of array1) {
+  if (array2.includes(element)) {
+  uniqieElements.push(element);
+  }
+  }
+
+return uniqieElements;
+ // Change code above this line
+}
+
+//===========Task 26===========
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  for (const item of order) {
+    total += item;
+  }
+
+  // Change code above this line
+  return total;
+}
+
+//===========Task 27===========
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  for (const number of numbers) {
+    
+
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+//===========Task 28===========
+
+const a = 3 % 3;
+const b = 4 % 3;
+const c = 11 % 8;
+const d = 12 % 7;
+const e = 8 % 6;
+
+//===========Task 29===========
+
+function getEvenNumbers(start, end) {
+   // Change code below this line
+
+const newArray = [];
+  
+  for (let i = start; i <= end; i += 1) {
+if (i % 2 === 0) {
+
+ newArray.push(i); 
+}
+}
+
+  return newArray;
+    // Change code above this line
+}*/
+  
+//===========Task 30===========
+
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+    
+   break; 
+  }
+}
+console.log(number);
+
+//===========Task 31===========
+
+function findNumber(start, end, divisor) {
+  // Change code below this line
+  
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      
+     return i;  
+    }
+  }
+}
+
+
+//===========Task 32===========
+
+function includes(array, value) {
+  // Change code below this line
+for (let i = 0; i < array.length; i += 1) {
+if (array[i] === value) {
+
+  return true;
+}
+}
+ return false; 
+  // Change code above this line
+}
