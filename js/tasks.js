7c49b4845efcdc9b34f76e734d2f11b6ea0f0b25
Tatiana Romanenko <t.womanly@gmail.com>
2021-05-28 23:36:10 +0300

    Модуль 3

--- js/tasks.js
+++ js/tasks.js
@@ -732,10 +732,10 @@ if (i % 2 === 0) {
   return newArray;
     // Change code above this line
 }*/
-  
+
 //===========Task 30===========
 
-const start = 6;
+/*const start = 6;
 const end = 27;
 let number;
 
@@ -775,4 +775,687 @@ if (array[i] === value) {
 }
  return false; 
   // Change code above this line
+}*/
+
+
+//===========MODUL 3===========
+
+//===========Task 1===========
+
+/*const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+console.log(apartment);
+
+//===========Task 2===========
+
+const apartment = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Spacious apartment in the city center',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+    owner: {
+      name: "Henry",
+      phone: "982-126-1588",
+      email: "henry.carter@aptmail.com",
+    }
+};
+  
+//===========Task 3===========
+
+const apartment = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Spacious apartment in the city centre',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+  };
+  
+  // Change code below this line
+  const aptRating = apartment.rating;
+  const aptDescr = apartment.descr;
+  const aptPrice = apartment.price;
+  const aptTags = apartment.tags;
+  // Change code above this line
+
+  //===========Task 4===========
+
+  const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[2];
+// Change code above this line
+
+//===========Task 5===========
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+};
+
+// Change code below this line
+const aptRating = apartment["rating"];
+const aptDescr = apartment["descr"];
+const aptPrice = apartment["price"];
+const aptTags = apartment["tags"];
+// Change code above this line
+
+//===========Task 6===========
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com'
+  }
+};
+
+// Change code below this line
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = "Henry Sibola";
+apartment.tags.push("trusted");
+
+//===========Task 7===========
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city centre',
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
+  owner: {
+    name: 'Henry Sibola',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com'
+  }
+};
+
+// Change code below this line
+
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {
+country: "Jamaica",
+city: "Kingston",   
+};
+
+//===========Task 8===========
+
+const name = 'Repair Droid';
+const price = 2500;
+const image = 'https://via.placeholder.com/640x480';
+const tags = ['on sale', 'trending', 'best buy'];
+
+const product = {
+  // Change code below this line
+name,
+price,
+image,  
+tags
+
+  // Change code above this line
+};
+
+//===========Task 9===========
+
+const emailInputName = 'email';
+const passwordInputName = 'password';
+
+const credentials = {
+  // Change code below this line
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam"
+  
+  
+  // Change code above this line
+};
+
+//===========Task 10===========
+
+const apartment = {
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (const key in apartment) {
+keys.push(key);
+values.push(apartment[key])
 }
+
+//===========Task 11===========
+
+const keys = [];
+const values = [];
+const advert = {
+  service: 'apt',
+};
+const apartment = Object.create(advert);
+apartment.descr = 'Spacious apartment in the city centre';
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  // Change code below this line
+if (apartment.hasOwnProperty(key)) {
+  keys.push(key);
+  values.push(apartment[key]);
+  
+}
+
+  // Change code above this line
+}
+
+
+//===========Task 12===========
+
+function countProps(object) {
+  let propCount = 0;
+  // Change code below this line
+  
+  for (const obj in object) {
+if (object.hasOwnProperty(obj)) {
+
+  propCount += 1;
+}
+  }
+  // Change code above this line
+  return propCount;
+}
+
+//===========Task 13===========
+
+const apartment = {
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Change code below this line
+const keys = Object.keys(apartment);
+
+for (const key of keys) {
+values.push(apartment[key]);
+}
+
+//===========Task 14===========
+
+function countProps(object) {
+  // Change code below this line
+  let propCount = 0;
+  
+  const keys = Object.keys(object);
+
+  for (const key of keys) {
+      propCount += 1;
+    }
+  
+
+  return propCount;
+  // Change code above this line
+}
+
+//===========Task 15===========
+const apartment = {
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
+
+//===========Task 16===========
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+const values = Object.values(salaries);
+  
+  for (const value of values) {
+  totalSalary += value;
+  }
+  // Change code above this line
+  return totalSalary;
+}*/
+
+//===========Task 17===========
+
+/*const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+for (const color of colors) {
+hexColors.push(color.hex);
+rgbColors.push(color.rgb);  
+};
+
+console.log(hexColors);
+console.log(rgbColors);
+
+
+//===========Task 18===========
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  
+  for (const product of products) {
+    
+  if (productName === product.name) {
+  return product.price;
+  } 
+    
+  }
+  return null;
+  
+  // Change code above this line
+}
+
+//===========Task 19===========
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  
+  const array = [];
+ 
+  for (const product of products) {
+  if (product.hasOwnProperty(propName)) {
+  
+  array.push(product[propName]);
+  }
+  }
+  
+  return array;
+  
+  
+  // Change code above this line
+}*/
+
+//===========Task 20===========
+
+/*const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+  let totalPrice = 0;
+  
+ for (const product of products) {
+    
+  if (productName === product.name) {
+  
+  totalPrice = product.price * product.quantity
+  }
+ }
+
+return totalPrice;
+  // Пиши код выше этой строки
+}
+
+//===========Task 21===========
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const { yesterday, today, tomorrow } = highTemperatures;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//===========Task 22===========
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//===========Task 23===========
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const { yesterday: highYesterday,
+     today: highToday, tomorrow: highTomorrow, icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures;
+
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+//===========Task 24===========
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (const { hex, rgb } of colors) {
+  
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+//===========Task 25===========
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+
+const {
+  today: { low: lowToday, high: highToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },
+  tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" }, } = forecast;
+
+  //===========Task 26===========
+
+  // Change code below this line
+function calculateMeanTemperature(forecast) {
+  const { today: { low: todayLow, high: todayHigh }, tomorrow: { low: tomorrowLow, high: tomorrowHigh }, } = forecast;
+
+  // Change code above this line
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+//===========Task 27===========
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Change code below this line
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+//===========Task 28===========
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+//===========Task 29===========
+const defaultSettings = {
+  theme: 'light',
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Change code below this line
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+//===========Task 30===========
+
+function makeTask(data) {
+  const completed = false;
+  const category = 'General';
+  const priority = 'Normal';
+  // Change code below this line
+const newData = {completed, category, priority, ...data};
+  
+  return newData; 
+  // Change code above this line
+}
+
+//===========Task 31===========
+
+// Change code below this line
+function add(...args) {
+  let value = 0;
+  for (arg of args) {
+  value += arg;
+  }
+  
+  return value;
+  // Change code above this line
+}
+
+//===========Task 32===========
+
+// Change code below this line
+function addOverNum(value, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    if (arg > value) {
+    total += arg;
+  }
+  }
+
+  return total;
+  // Change code above this line
+}
+
+//===========Task 33===========
+
+// Change code below this line
+function findMatches(...args) {
+  const matches = []; // Don't change this line
+
+  // Change code above this line
+  
+  for (let i = 1; i < args.length; i += 1) {
+    if (args[0].includes(args[i])) {
+    
+    matches.push(args[i]);
+    }
+  
+  }
+  return matches;
+}
+
+//===========Task 34===========
+
+const bookShelf = {
+  // Change code below this line
+  books: ['The last kingdom', 'The guardian of dreams'],
+  getBooks() {
+    return 'Returning all books';
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+  return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+  return `Updating book ${oldName} to ${newName}`;
+  },
+  // Change code above this line
+};
+
+//===========Task 35===========
+const bookShelf = {
+  books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
+  updateBook(oldName, newName) {
+    // Change code below this line
+	const bookIndex = this.books.indexOf(oldName);
+	this.books.splice(bookIndex, 1, newName);
+	return bookShelf.updateBook;
+    // Change code above this line
+  },
+};
+
+//===========Task 36===========
+
+const atTheOldToad = {
+  // Change code below this line
+potions: [],
+  // Change code above this line
+};
+
+//===========Task 37===========
+
+const atTheOldToad = {
+  // Change code below this line
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  getPotions() {
+    return this.potions;
+  },
+  // Change code above this line
+};
+
+//===========Task 38===========
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  addPotion(potionName) {
+    // Change code below this line
+    this.potions.push(potionName);
+    // Change code above this line
+  },
+};
+
+//===========Task 39===========
+
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  removePotion(potionName) {
+    // Change code below this line
+   const potionIndex = this.potions.indexOf(potionName);
+    return this.potions.splice(potionIndex, 1);
+    // Change code above this line
+  },
+};
+
+//===========Task 40===========
+
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  updatePotionName(oldName, newName) {
+    // Change code below this line
+ const potionIndex = this.potions.indexOf(oldName);
+	return this.potions.splice(potionIndex, 1, newName);
+    // Change code above this line
+  },
+};*/
+
+//===========Task 41===========
+
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(potionName) {
+    const { potions } = this;
+    const { name } = potionName;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === name) {
+        return `Potion ${name} is already equipped!`;
+      }
+      potions.push(potionName);
+    }
+  },
+  removePotion(potionName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === potionName) {
+        potions.splice(i, 1);
+        return potions;
+      }
+    }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+  updatePotionName(oldName, newName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === oldName) {
+        potions[i].name = newName;
+        return potions;
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
+  // Change code above this line
+};
